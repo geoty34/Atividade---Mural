3af6879e93e24544dfa15eae57076be117604586
Geovana Santos <101215198+geoty34@users.noreply.github.com>
2023-11-13 17:43:36 -0300

    Meia boca

---
+++ mural_de_recados-main/App/Controllers/RecadosController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controllers;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use App\Repositories\RecadosRepository;
+
+class RecadosController {
+
+    private mixed $container;
+    
+    private RecadosRepository $repository;
+
+    public function __construct($container)
+    {
+
+        $this->container = $container;
+        $this->repository = new RecadosRepository();
+    }
+    
+    //Adicione seus métodos aqui:
+
+    public function obterTodos(Request $request, Response $response, array $params){
+
+        $inicio = $request->getParam('inicio');
+        $inicio = isset($inicio) ? $inicio : '2000-01-01';
+        $data = $this->repository->obterTodos($inicio);
+        return $response->withJson($data);
+    }
+
+    public function cadastrar(Request $request, Response $response, array $params){
+        $recado = $request->getParsedBody();
+        $recado = $this->repository->cadastrar($recado);
+        return $response->withJson($recado);
+    }
+
+    public function remover(Request $request, Response $response, array $params){
+        $id = $params['id'];
+        $recado = $this->repository->remover($id);
+        return $response->withJson($recado);
+    }
+    
+
+}
